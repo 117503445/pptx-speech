8eaf4cb0012b25183a1edf5a2ba1629025503d8d
117503445 <t117503445@gmail.com>
2023-07-31 12:52:39 +0800

    feat: tts retry
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- src/pptx-speech/pptx_speech/tts/azure.py
+++ src/pptx-speech/pptx_speech/tts/azure.py
@@ -1,11 +1,14 @@
 import azure.cognitiveservices.speech as speechsdk
 from pathlib import Path
+from tenacity import retry, stop_after_attempt
+
 
 class AzureTTS:
     def __init__(self, key: str, region: str):
         self.speech_config = speechsdk.SpeechConfig(subscription=key, region=region, speech_recognition_language='zh')
         self.speech_config.speech_synthesis_voice_name='zh-CN-YunxiNeural'
 
+    @retry(stop=stop_after_attempt(3))
     def tts(self, text: str, filename: Path):
         audio_config = speechsdk.audio.AudioOutputConfig(filename=str(filename.absolute()))
         speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=self.speech_config, audio_config=audio_config)
@@ -15,9 +18,11 @@ class AzureTTS:
             print("Speech synthesized for text [{}]".format(text))
         elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
             cancellation_details = speech_synthesis_result.cancellation_details
-            print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+            
             if cancellation_details.reason == speechsdk.CancellationReason.Error:
                 if cancellation_details.error_details:
-                    print("Error details: {}".format(cancellation_details.error_details))
+                    raise Exception("Azure tts failed: {}".format(cancellation_details.error_details))
+                
+            raise Exception("Azure tts failed: {}".format(cancellation_details.reason))
         else:
-            raise Exception("Unknown reason: {}".format(speech_synthesis_result.reason))
+            raise Exception("Azure tts failed: {}".format(speech_synthesis_result.reason))
